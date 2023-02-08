ed7347ee9e47acfbe7e11f8a429f95b0d4cd1a8c
Perry Raleigh <perry.raleigh@gmail.com>
2023-02-07 21:36:23 -0700

    Week 06 completed tutorial

--- RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages.Movies
@@ -25,18 +19,17 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         [BindProperty]
-        public Movie Movie { get; set; }
+        public Movie? Movie { get; set; }
         
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-            _context.Movie.Add(Movie);
+            _context.Movie.Add(Movie!);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
