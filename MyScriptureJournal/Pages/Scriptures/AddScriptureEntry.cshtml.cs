59cdab2190a285fbd72515135c010045b3d281cf
Perry Raleigh <perry.raleigh@gmail.com>
2023-02-17 20:16:45 -0700

    Sync up while it's stable

---
+++ MyScriptureJournal/Pages/Scriptures/AddScriptureEntry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class AddScriptureEntryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyScriptureJournal/Pages/Scriptures/ViewAllScriptureEntries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class ViewAllScriptureEntriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
