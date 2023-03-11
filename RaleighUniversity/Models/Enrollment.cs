a2b26899d5b68d525799f3d17ca69ebfbd8afd6a
Perry Raleigh <perry.raleigh@gmail.com>
2023-03-11 00:36:36 -0700

    Week 10 Sync at the end of the night
    Week 10 Sync at the end of the night

--- RaleighUniversity/Models/Enrollment.cs
+++ RaleighUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace RaleighUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaleighUniversity.Models
 {
     public enum Grade
     {
@@ -8,11 +10,16 @@
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
+
         public int CourseID { get; set; }
+
         public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
+
         public Student Student { get; set; }
     }
 }
